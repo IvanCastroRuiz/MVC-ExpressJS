c54834d641ec34023652a03007a272638eebc4f8
Diana <dianapatino82@gmail.com>
2022-04-27 11:39:28 -0500

    configuracion cors

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ import router from './router/router.js';
 
 const port = process.env.port || 4000;
 const host = process.env.HOST || '0.0.0.0';
-dotenv.config({ path: '.env' });
+//dotenv.config({ path: '.env' });
+dotenv.config();
 const app = express();
 
 
 // middlewares
 // Se utiliza para realizar la comunicacion entre el servidor del frontend y el backend
-app.use(cors());
-app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+/*const dominiosPermitidos = [process.env.FRONTEND_URL];
+const corsOptions = {
+    origin: function(origin, callback){
+        if(dominiosPermitidos.indexOf(origin) !== -1){
+            // El origen del Request esta permitido
+            callback(null, true);
+        }else{
+            callback(new error('No permitido por CORS'));
+        }
+    }
+}*/   
+
+//app.use(cors(corsOptions)); 
+app.use(cors()); 
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', router);
 
